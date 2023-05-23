6b6a142f3aefb87b6c91f842b2cd29a4869a0a45
freshcodeAlice <freshjs.ofte@gmail.com>
2023-05-23 11:25:09 +0300

    create n insert

--- fm-test.session.sql
+++ fm-test.session.sql
@@ -30,3 +30,117 @@ $ - або для ідентифікатора (хоча не бажано)
 
 
 
+/* SQL-команди 
+
+- DDL - Data Definition Language (визначення)
+Create,
+Drop,
+Alter,
+Truncate
+Rename
+
+- DML - Data Manipulation Language (управління)
+Insert,
+Update,
+Delete,
+Call,
+Explain,
+Lock
+
+- DQL - Data Query Language - (запити)
+SELECT
+
+- TCL - Transaction Control Language
+Commit
+Rollback
+Savepoint
+(set transaction)
+(set constraint)
+
+- DCL - Data Control Language (управління доступом)
+Grant
+Revoke
+*/
+ 
+
+
+/* CREATE TABLE */
+
+/*
+CREATE TABLE ім'я_таблиці(
+    ім'я_стовпця1 тип_даних .....,
+    ім'я_стовпця2 тип_даних .....,
+    ім'я_стовпця3 тип_даних .....,
+    ім'я_стовпця4 тип_даних .....
+) 
+В одній таблиці не може бути двох одноіменних стопвців
+
+*/
+
+
+/*
+Символьні типи даних
+char()
+varchar()
+text - Рядок символів (великий). Максимальний розмір - 1GB
+
+char(5) - Рядок фіксованої довжини
+'12345' - ок
+'123  ' - якщо коротший, то заб'є пробілами
+'12345 67' - якщо значення більше, його вкоротять
+
+varchar(5) - Рядок, що може зменшуватись, якщо дані менше заявленої довжини
+'12345' - ok
+'123' - ок, не буде доповнювати пробілами
+'12345 67' - якщо значення більше, його вкоротять
+
+*/
+
+
+/* Числові типи даних 
+decimal, numeric
+numeric(точність, масштаб)
+
+Точність - загальна кількість цифр в числі
+Масштаб - кількість цифр після точки
+
+
+Наприклад.
+Число 234.09 
+точність - 5
+масштаб - 2
+
+Число 0.123763
+точність - 7
+масштаб - 6
+
+*/
+
+CREATE TABLE users(
+    first_name varchar(300),
+    last_name varchar(300),
+    email varchar(500),
+    age date,
+    is_subscribe boolean,
+    height numeric(3, 2),
+    weight int
+);
+
+/* 1.95 або 0.80 або 3.20 */
+
+
+/* INSERT INTO ім'я_таблиці VALUES (перерахування всіх значень в тому порядку, в якому ідуть стовпці)*/
+
+INSERT INTO users VALUES 
+('Thor', 'Odinsson', 'super@avenger.com', '1990-09-02', true, 2.80, 90);
+
+
+INSERT INTO users VALUES
+('Tony', 'Stark', 'vip@ave.com', '2000-01-01', true, NULL, 70), 
+('Bruce', 'Benner', NULL, '1980-01-23', false, 4.50, 200), 
+('Peter', 'Parker', 'spider@man', '1995-01-02', false, NULL, NULL);
+
+
+/* Помилка - неправильний тип даних! */
+INSERT INTO users VALUES 
+(true, 'Odinsson', 'super@avenger.com', '1990-09-02', 'not subscribe', 90);
