084b245a81b6b957b68c721cfe8e9b972679d832
freshcodeAlice <freshjs.ofte@gmail.com>
2023-06-02 09:36:10 +0300

    agregations

--- sql/agregations.sql
+++ sql/agregations.sql
@@ -13,20 +13,56 @@ SELECT email
 FROM users;
 
 
-/*------- */
+/* Агрегатні функції */
 
-UPDATE workers
-SET birthday = birthday + MAKE_INTERVAL(
-    years => 1999 - EXTRACT(
-      year
-      from birthday
-    )::INTEGER
-  )
-WHERE id BETWEEN 2 AND 5;
+--- Задача - визначити суму ваги всіх користувачів
+
+SELECT sum(weight) 
+FROM users;
 
 
--------
+--- Задача - визначити найменший зріст серед всіх користувачів
 
-UPDATE workers
-SET birthday = '1999-01-02'
-WHERE id BETWEEN 2 AND 5;
+SELECT min(height)
+FROM users;
+
+
+/*
+max - максимальне
+min - мінімальне
+sum - сума
+count - кількість значень
+avg - среднє арифметичне
+
+*/
+
+SELECT avg(weight)
+FROM users;
+
+
+--- Кількість користувачів
+
+SELECT count(*)
+FROM users;
+
+
+--- Середня вага чоловіків та середня вага жінок
+
+SELECT avg(weight), gender
+FROM users
+GROUP BY gender;
+
+
+--- Порахувати середню вагу всіх користувачів, які народились після 2000 року
+
+SELECT avg(weight)
+FROM users
+WHERE extract(years from birthday) > 2000; 
+
+
+--- Середній зріст всіх чоловіків, яким понад 25 років
+
+SELECT avg(height), gender
+FROM users
+WHERE extract(years from age(birthday)) > 25
+GROUP BY gender;
