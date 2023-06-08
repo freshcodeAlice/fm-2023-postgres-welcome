37e95fdceb2cedecfb3ab06a77868550226db206
freshcodeAlice <freshjs.ofte@gmail.com>
2023-06-08 10:15:09 +0300

    2NF

--- sql/normilize.sql
+++ sql/normilize.sql
@@ -11,4 +11,53 @@ INSERT INTO test VALUES ('test', 1),
 ('test', 3);
 
 
------
+--2NF---
+
+
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name varchar(300),
+    position varchar(300),
+    car_aviability boolean
+);
+
+INSERT INTO employees (name, position, car_aviability) VALUES
+('John', 'HR', false),
+('Jane', 'Sales',  false),
+('Jake', 'Developer', false),
+('Andrew', 'driver', true); ---- not 2NF! need refactor!
+
+
+----> 
+
+CREATE TABLE positions (
+    name varchar(300) PRIMARY KEY,
+    car_aviability boolean
+);
+
+DROP TABLE employees;
+
+CREATE TABLE employees (
+    id serial PRIMARY KEY,
+    name varchar(300),
+    position varchar(300) REFERENCES positions(name)
+);
+
+INSERT INTO positions VALUES
+('HR', false),
+('Sales', false),
+('Developer', false),
+('Driver', true);
+
+INSERT INTO employees (name, position) VALUES
+('John', 'HR'),
+('Jane', 'Sales'),
+('Jake', 'Developer'),
+('Andrew', 'Driver');
+
+
+--- Як побачити інфу про співробітника + інфу про слуюбову машину
+
+SELECT * FROM employees
+JOIN positions
+ON employees.position = positions.name;
