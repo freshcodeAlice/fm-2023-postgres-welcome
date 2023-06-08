dbcb2ed38d4d36904c73f98335e606d41fa6b7dd
freshcodeAlice <freshjs.ofte@gmail.com>
2023-06-08 10:47:03 +0300

    3NF

--- sql/normilize.sql
+++ sql/normilize.sql
@@ -61,3 +61,30 @@ INSERT INTO employees (name, position) VALUES
 SELECT * FROM employees
 JOIN positions
 ON employees.position = positions.name;
+
+
+
+
+---3NF------
+
+CREATE TABLE employees(
+    id serial PRIMARY KEY,
+    name varchar(300),
+    department varchar(300),
+    department_phone int
+);
+
+
+INSERT INTO employees (name, department, department_phone) VALUES
+('John Dow', 'HR', 44455),
+('Jane Smith', 'sales', 33322),
+('Andrew', 'HR', 44455);
+
+
+/* departmant -> id, department_phone -> department,
+транзитивна залежність: department_phone -> id */
+
+CREATE TABLE departments (
+    name varchar(300) PRIMARY KEY,
+    phone_number int
+);
